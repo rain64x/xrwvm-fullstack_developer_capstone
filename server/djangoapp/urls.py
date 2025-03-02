538630295d095e62de652651b29478335b76d2ad
Mahesh Anand Kadri <mahesh@rafled.com>
2025-03-02 04:34:01 -0500

    car models added, views added

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,8 +1,9 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
+from . import views
+
 from django.conf import settings
-# from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
@@ -10,6 +11,9 @@ urlpatterns = [
 
     # path for login
     # path(route='login', view=views.login_user, name='login'),
+
+    # path for get cars view
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for dealer reviews view
 
